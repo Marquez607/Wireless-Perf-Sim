4d6a671020a497fcd502b43eebad778038af78c2
marquez607 <marquezjones607@gmail.com>
2022-04-01 16:28:42 -0400

    blank script for start, will need to base off of global routing

---
+++ ns-3-dev/scratch/IoT-Latency-Sim/IoT-Latency-Sim.cc
@@ -0,0 +1,34 @@
+/*************************************************
+ * Name: IoT Latency Sim
+ * Author: Marquez Jones
+ * Desc: This simulation is designed to test End-to-End delay 
+ *       for multihop mesh system with the goal of providing a tool 
+ *       that analyzes hardware impact on the network design for IoT
+ *       Applications 
+ * 
+ ************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("IoT Latency Sim");
+
+int
+main(int argc,char *argv[]){
+
+
+
+    return 0;
+}
